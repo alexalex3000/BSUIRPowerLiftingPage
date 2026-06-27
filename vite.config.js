d7fc12cd80488cc663a172a5911313ff26148d8e
Alexei Petrikevitch <petrikevitch.alexei@yandex.by>
2026-06-27 20:04:34 +0300

    v1.01

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import path from 'path'
 
 export default defineConfig({
     plugins: [react(), svgr()],
-    base: '/BSUIRPowerLiftingPage/',
     css: {
         preprocessorOptions: {
             scss: {
