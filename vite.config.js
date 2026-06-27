19f4a2d0eea316d3d40e01bf9540f0fcccce592f
Alexei Petrikevitch <petrikevitch.alexei@yandex.by>
2026-06-27 20:26:38 +0300

    Fix mode reference error and sass imports

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import path from 'path'
 
-export default defineConfig(() => {
+export default defineConfig(({mode}) => {
     const isProd = mode === 'production'
 
     return {
