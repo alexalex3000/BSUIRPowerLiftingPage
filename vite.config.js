8bf2c020650570a18b0816c502756cc986aa0593
Alexei Petrikevitch <petrikevitch.alexei@yandex.by>
2026-06-27 20:24:23 +0300

    Fix build and deploy configuration

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,28 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import path from 'path'
 
-export default defineConfig({
-    plugins: [react(), svgr()],
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: (content, filename) => {
-                    // Добавляем все технические файлы в исключения, чтобы они не импортировали сами себя
-                    if (
-                        filename.includes('_shared.scss') ||
-                        filename.includes('_variables.scss') ||
-                        filename.includes('_fonts.scss') ||
-                        filename.includes('_mixins.scss')
-                    ) {
-                        return content;
+export default defineConfig(() => {
+    const isProd = mode === 'production'
+
+    return {
+        plugins: [react(), svgr()],
+        base: isProd ? '/BSUIRPowerLiftingPage/' : '/',
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: (content, filename) => {
+                        // Добавляем все технические файлы в исключения, чтобы они не импортировали сами себя
+                        if (
+                            filename.includes('_shared.scss') ||
+                            filename.includes('_variables.scss') ||
+                            filename.includes('_fonts.scss') ||
+                            filename.includes('_mixins.scss')
+                        ) {
+                            return content;
+                        }
+                        const sharedPath = path.resolve(__dirname, 'src/styles/_shared.scss').replace(/\\/g, '/');
+                        return `@import "${sharedPath}";\n${content}`;
                     }
-                    const sharedPath = path.resolve(__dirname, 'src/styles/_shared.scss').replace(/\\/g, '/');
-                    return `@import "${sharedPath}";\n${content}`;
                 }
             }
         }
