f7e7c52af00fee56cb4a889a3fb07a9d01633438
Alexei Petrikevitch <petrikevitch.alexei@yandex.by>
2026-06-27 19:38:08 +0300

    Configure Vite and GitHub Actions for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 export default defineConfig({
     plugins: [react(), svgr()],
+    base: '/BSUIRPowerLiftingPage/',
     css: {
         preprocessorOptions: {
             scss: {
